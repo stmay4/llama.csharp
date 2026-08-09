cb435d9306f4968a8f89993b0584f1fe5a53268d
Станислав Майоров <stasmayorov2004@mail.ru>
2026-08-10 00:21:09 +0300

    Добавлена структура для хранения MROPE позиций

---
+++ llama.csharp/Native/MtmdDecoderPosNative.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Llama.csharp.Native
+{
+    /// <summary>
+    /// C++ mtmd_decoder_pos
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MtmdDecoderPosNative
+    {
+        uint t;
+        uint x;
+        uint y;
+        uint z; // unused for now, reserved for future use
+    }
+}
